43e39ef75ec32c6ba78501924cb3a680d53b08d3
gabriel.jesus <gabriel.jesus@animaeducacao.com.br>
2022-08-21 21:49:24 -0300

    Adicionar arquivos de projeto.

---
+++ QuickBuy/QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,30 @@
+﻿using QuickBuy.Dominio.Enums;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+
+        public bool ECartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito]; }
+        }
+
+        public bool EDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
